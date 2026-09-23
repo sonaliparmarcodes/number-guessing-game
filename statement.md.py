55bdf5857008002c75fdf033e597e204133f7002
Sonali Parmar <sonaliparmar097@gmail.com>
2026-09-23 19:50:26 +0530

    Add files via upload

---
+++ statement.md.py
@@ -0,0 +1,15 @@
+NUMBER GUESSING GAME
+PROBLEM STATEMENT
+
+Develop a Python program to create an interactive Number Guessing Game in which the computer randomly generates a number within the specified range,and the
+user attempts to guess the generated number.
+
+The program should ask the user to entera guess and compare it with the randomly generated number. If the user's guess is greater than the generated number,
+the program should display a messeage indicating that the guess is too high. If the guess is smaller than the generated number,the program should indicate
+the guess is too low.if the user guesses the correct number,the program should display a congratulatory messege.
+
+the program should continue accepting guesses until the user correctly identifies the number.It should also keep  track of the number of attempts made by the
+user and display the total number of attempts made by the user and display the total number of attempts after the correct guess.
+
+the objective of this program is to demonstrate the use of random number generation,variables,conditional statements,loops,user input, basic Python
+programming concepts. 
